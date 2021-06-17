f7c040563f63c87e81cab6d5adf0b024bd5516ef
Hai Lam Dang <hai.dang@ue-germany.de>
2021-06-17 19:44:07 +0200

    [Change] Fix Player/CameraMovement, Add Highlight Function

---
+++ P2_Movement/Assets/Scripts/UI/SelectionManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    //Differenciate Selectable and non-Selectable through Tags
+    [SerializeField]
+    private string selectableTag; //now all GO with this tag can be highlight ^^
+
+    //Assign highlightMaterial
+    [SerializeField]
+    private Material highlightMaterial;
+    
+    [SerializeField]
+    private Material defaultMaterial;
+
+    //Save current selection
+    private Transform _selection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //De-selection -> GO goes back to defaultMaterial
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial; //set material to default material
+            _selection = null;
+        }
+
+        //Assign a ray
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //Object being hit
+        RaycastHit hit;
+
+        //check if Ray hit the object -> Selection
+        if (Physics.Raycast(ray, out hit))
+        {
+            //Selected object
+            var selection = hit.transform;
+
+            //Check through tag if GO can be highlighted
+            if (selection.CompareTag(selectableTag))
+            {
+                var selectionRenderer = selection.GetComponent<Renderer>();
+
+                //check if selected Object doesn't NOT have a renderer
+                if(selectionRenderer != null )
+                {
+                    //if true -> set default Material to highlightMaterial
+                    selectionRenderer.material = highlightMaterial;
+                }
+
+                _selection = selection;
+            }
+        }
+    }
+}
