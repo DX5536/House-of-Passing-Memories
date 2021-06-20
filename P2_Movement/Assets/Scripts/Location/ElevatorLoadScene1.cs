aea75daf8867e483b2034c8856918a736019bc0a
Hai Lam Dang <hai.dang@ue-germany.de>
2021-06-20 23:27:01 +0200

    [Change] Add Elevator from GroundFloor to Basement

--- P2_Movement/Assets/Scripts/Location/ElevatorLoadScene1.cs
+++ P2_Movement/Assets/Scripts/Location/ElevatorLoadScene1.cs
@@ -3,28 +3,32 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class ElevatorLoadScene : MonoBehaviour
+public class ElevatorBasementLoadScene : MonoBehaviour
 {
     [SerializeField]
     private string sceneName;
 
     [SerializeField]
-    private string basementSceneName;
+    private string sceneBasement;
 
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Elevator"))
         {
+            if (other.CompareTag("ElevatorBasement"))
+            {
+                SceneManager.LoadScene(sceneBasement);
+                Debug.Log("To the basement!");
+            }
+
             //In Ground Floor: Collide with Elevator -> Go to Attic
             SceneManager.LoadScene(sceneName);
             Debug.Log("Should be teleported");
+
         }
 
-        else if (other.CompareTag("ElevatorBasement"))
-        {
-            SceneManager.LoadScene(basementSceneName);
-        }
+        
 
         
 
